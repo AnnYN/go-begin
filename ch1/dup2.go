776d3c64fa749a35dd9d01d10470fd667ed5d383
yanan <liuyanan2017@126.com>
2019-04-27 12:44:30 +0800

    gobook code

---
+++ ch1/dup2.go
@@ -0,0 +1,56 @@
+//Dup2 prints the count and text of line that appear more than once 
+// in the input. It reads from stdin or from named files.
+
+package main
+
+import( 
+
+"bufio"
+"fmt"
+"os"
+
+)
+
+
+func main(){
+
+counts := make(map[string]int)
+files := os.Args[1:]
+if len(files) == 0{
+countLines(os.Stdin, counts)
+}else{
+for _, arg := range files{
+f, err := os.Open(arg)
+if err != nil{
+fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+continue
+}
+countLines(f,counts)
+f.Close()
+
+}
+
+
+}
+
+for line, n := range counts{
+if n >1 {
+fmt.Printf("%d\t%s\n",n,line)
+}
+
+
+
+}
+
+
+}
+
+
+func countLines(f *os.File, counts map[string]int){
+input := bufio.NewScanner(f)
+for input.Scan(){
+counts[input.Text()]++
+
+}
+}
+//NOTE: ignoring potential errors
